bcfcd95527c8f4edf9bc5bc9191fae58376f4bfb
MateoCF <mateojc2@illinois.edu>
2024-04-23 13:06:18 -0500

    working on finding nearest coord

---
+++ data.js
@@ -0,0 +1,18 @@
+let data = {
+    "union": {
+        "lat": 1, 
+        "lng": 1, 
+    },
+    "library": {
+        "lat": 2, 
+        "lng": 2, 
+    },
+    "cif": {
+        "lat": 3, 
+        "lng": 3, 
+    },
+    "grainger": {
+        "lat": 4, 
+        "lng": 4, 
+    }
+}
--- script.js
+++ script.js
@@ -1,80 +1,117 @@
 const compassCircle = document.querySelector(".compass-circle");
-    const myPoint = document.querySelector(".my-point");
-    const startBtn = document.querySelector(".start-btn");
-    const isIOS =
-      navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
-      navigator.userAgent.match(/AppleWebKit/);
+const myPoint = document.querySelector(".my-point");
+const startBtn = document.querySelector(".start-btn");
+const isIOS =
+  navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
+  navigator.userAgent.match(/AppleWebKit/);
+personalCoord = [0,0]
+let locationList = [];
+var nearestCoord = [];
 
-    function init() {
-      startBtn.addEventListener("click", startCompass);
-      navigator.geolocation.getCurrentPosition(locationHandler);
+// Helper functions 
+for (let location in data) {
+  locationList.push([
+    data[location]["lat"],
+    data[location]["lng"]
+  ])
+}
 
-      if (!isIOS) {
-        window.addEventListener("deviceorientationabsolute", handler, true);
+function distance(coord1, coord2) {
+  return Math.sqrt(Math.pow(coord1[0] - coord2[0], 2) + Math.pow(coord1[1] - coord2[1], 2));
+}
+
+function nearestCoordinate(personal, coordinates) {
+  let minDistance = Infinity;
+  let nearestCoord = null;
+
+  for (let coord of coordinates) {
+      let dist = distance(personal, coord);
+      if (dist < minDistance) {
+          minDistance = dist;
+          nearestCoord = coord;
       }
-    }
+  }
 
-    function startCompass() {
-      if (isIOS) {
-        DeviceOrientationEvent.requestPermission()
-          .then((response) => {
-            if (response === "granted") {
-              window.addEventListener("deviceorientation", handler, true);
-            } else {
-              alert("has to be allowed!");
-            }
-          })
-          .catch(() => alert("not supported"));
-      }
-    }
+  return nearestCoord;
+}
 
-    function handler(e) {
-      compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
-      compassCircle.style.transform = `translate(-50%, -50%) rotate(${-compass}deg)`;
 
-      // ±15 degree
-      if (
-        (pointDegree < Math.abs(compass) &&
-          pointDegree + 15 > Math.abs(compass)) ||
-        pointDegree > Math.abs(compass + 15) ||
-        pointDegree < Math.abs(compass)
-      ) {
-        myPoint.style.opacity = 0;
-      } else if (pointDegree) {
-        myPoint.style.opacity = 1;
-      }
-    }
+// Starting function
+function init() {
+  // Awaits for permissions and adds handler 
+  startBtn.addEventListener("click", startCompass);
+  // Main deal 
+  navigator.geolocation.getCurrentPosition(locationHandler);
 
-    let pointDegree;
+  // IOS requires specific handler, android automatically does this in first line of this func
+  if (!isIOS) {
+    window.addEventListener("deviceorientationabsolute", handler, true);
+  }
+}
 
-    function locationHandler(position) {
-      const { latitude, longitude } = position.coords;
-      pointDegree = calcDegreeToPoint(latitude, longitude);
+function startCompass() {
+  if (isIOS) {
+    DeviceOrientationEvent.requestPermission()
+      .then((response) => {
+        if (response === "granted") {
+          window.addEventListener("deviceorientation", handler, true);
+        } else {
+          alert("has to be allowed!");
+        }
+      })
+      .catch(() => alert("not supported"));
+  }
+}
 
-      if (pointDegree < 0) {
-        pointDegree = pointDegree + 360;
-      }
-    }
+function handler(e) {
+  // Compass heavy lifting 
+  compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
+  compassCircle.style.transform = `translate(-50%, -50%) rotate(${-compass}deg)`;
 
-    function calcDegreeToPoint(latitude, longitude) {
-      // Starting location / Union
-      const point = {
-        lat: 40.109470,
-        lng: -43.22709 // +45deg to compensate for deterministic error? 
-      };
+  // At our goal ±15 degree
+  if (
+    (pointDegree < Math.abs(compass) &&
+      pointDegree + 15 > Math.abs(compass)) ||
+    pointDegree > Math.abs(compass + 15) ||
+    pointDegree < Math.abs(compass)
+  ) {
+    myPoint.style.opacity = 0;
+  } else if (pointDegree) {
+    myPoint.style.opacity = 1;
+  }
+}
 
-      const phiK = (point.lat * Math.PI) / 180.0;
-      const lambdaK = (point.lng * Math.PI) / 180.0;
-      const phi = (latitude * Math.PI) / 180.0;
-      const lambda = (longitude * Math.PI) / 180.0;
-      const psi =
-        (180.0 / Math.PI) *
-        Math.atan2(
-          Math.sin(lambdaK - lambda),
-          Math.cos(phi) * Math.tan(phiK) -
-            Math.sin(phi) * Math.cos(lambdaK - lambda)
-        );
-      return Math.round(psi);
-    }
+let pointDegree;
+function locationHandler(position) {
+  const { latitude, longitude } = position.coords;
+  
+  nearestCoord = nearestCoordinate(position, locationList);
 
-    init();
+  const desiredpoint = {
+    lat: nearestCoord[0],
+    lng: nearestCoord[1] 
+  };
+
+  pointDegree = calcDegreeToPoint(desiredpoint, latitude, longitude);
+
+  if (pointDegree < 0) {
+    pointDegree = pointDegree + 360;
+  }
+}
+
+function calcDegreeToPoint(desiredpoint, latitude, longitude) {
+  const phiK = (point.lat * Math.PI) / 180.0;
+  const lambdaK = (point.lng * Math.PI) / 180.0;
+  const phi = (latitude * Math.PI) / 180.0;
+  const lambda = (longitude * Math.PI) / 180.0;
+  const psi =
+    (180.0 / Math.PI) *
+    Math.atan2(
+      Math.sin(lambdaK - lambda),
+      Math.cos(phi) * Math.tan(phiK) -
+        Math.sin(phi) * Math.cos(lambdaK - lambda)
+    );
+  return Math.round(psi);
+}
+
+init();
