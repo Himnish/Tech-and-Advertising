192a9d181018bd311babd3b41e9bffcd6c7ce9b5
MateoCF <mateojc2@illinois.edu>
2024-04-25 15:42:00 -0500

    Added location based msgs

--- data.js
+++ data.js
@@ -2,17 +2,21 @@ let data = {
     "Union Fountain": {
         "lat": 40.109470, 
         "lng": -88.227090, 
+        "msg": "Union test"
     },
     "Main Library": {
         "lat": 40.104250, 
         "lng": -88.227250, 
+        "msg": "Main test"
     },
     "CIF": {
         "lat": 40.112720, 
         "lng": -88.227737, 
+        "msg": "CIF test"
     },
     "Grainger": {
         "lat": 40.110500, 
         "lng": -88.226349, 
+        "msg": "Grainger test"
     }
 }
