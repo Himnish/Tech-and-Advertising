f09f245a26a46714ae4c986a7cda952f84ea7b0e
MateoCF <mateojc2@illinois.edu>
2024-05-01 09:15:45 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,22 +1,27 @@
 let data = {
-    "ISR Fountain #3": {
+    "Union Fountain": {
         "lat": 40.109470, 
         "lng": -88.227090, 
-        "msg": "Found ISR #3"
+        "msg": "Union test"
+    },
+    "Main Library": {
+        "lat": 40.104250, 
+        "lng": -88.227250, 
+        "msg": "Main test"
+    },
+    "CIF": {
+        "lat": 40.112720, 
+        "lng": -88.227737, 
+        "msg": "CIF test"
+    },
+    "Grainger": {
+        "lat": 40.110500, 
+        "lng": -88.226349, 
+        "msg": "Grainger test"
+    },
+    "Gregory Hall": {
+        "lat": 40.105820, 
+        "lng": -88.228430, 
+        "msg": "Gregory test" 
     }
-    // "Main Library": {
-    //     "lat": 40.104250, 
-    //     "lng": -88.227250, 
-    //     "msg": "Main test"
-    // },
-    // "CIF": {
-    //     "lat": 40.112720, 
-    //     "lng": -88.227737, 
-    //     "msg": "CIF test"
-    // },
-    // "Grainger": {
-    //     "lat": 40.110500, 
-    //     "lng": -88.226349, 
-    //     "msg": "Grainger test"
-    // }
 }
